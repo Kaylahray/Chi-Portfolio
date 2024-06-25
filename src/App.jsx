5c43423334a9ca95ef7758051b5cb9265740a22e
chioma <chiomachristopher91@gmail.com>
2024-06-25 12:17:58 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import styles from "./App.module.css";
-import "@fontsource/outfit";
-import "@fontsource/roboto";
-// import { About } from "./Components/About/About"
-import { Contact } from "./Components/Contact/Contact";
-import { Experience } from "./Components/Experience/Experience";
-import { Hero } from "./Components/Hero/Hero";
-import { Navbar } from "./Components/NavBar/NavBar";
-import { Projects } from "./Components/Projects/Projects";
-
 const App = () => {
-  return (
-    <div className={styles.App}>
-      <Navbar />
-      <Hero />
-      <Experience />
-      <Projects />
-      <Contact />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
